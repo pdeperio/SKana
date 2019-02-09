ab638f31d9a7adb91e7d83cc17fbf4af94116073
Patrick de Perio <pdeperio@gmail.com>
2019-02-09 15:29:09 +0900

    Generalize working directory

--- skroot/sub_ana.sh
+++ skroot/sub_ana.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -f
 
+WORKDIR=${PWD}
 
 RUNS=(80249 80254 80263 80265 80269 80275 80278 80282)
 HV_SET=(0    +75   +50   +25    0    -25   -50   -75 )
 
 CONNECTION_DIR=/disk02/usr6/pdeperio/precalib_2018
+
 ihv=0
 for hv in ${HV_SET[@]}; do
 
@@ -21,13 +23,13 @@ done
 
 mkdir -p ./hv_ana
 
-SCRIPT_DIR=hv_ana_script
+SCRIPT_DIR=${WORKDIR}/hv_ana_script
 mkdir -p ${SCRIPT_DIR}
 
-LOG_DIR=hv_ana_log
+LOG_DIR=${WORKDIR}/hv_ana_log
 mkdir -p ${LOG_DIR}
 
-ERR_DIR=hv_ana_err
+ERR_DIR=${WORKDIR}/hv_ana_err
 mkdir -p ${ERR_DIR}
 
 
@@ -41,7 +43,7 @@ for run in ${RUNS[@]}; do
 source /usr/local/sklib_gcc4.8.5/skofl-trunk/env.sh
 hostname
 
-cd /disk02/usr6/pdeperio/190204-LD_analysis/skroot
+cd ${WORKDIR}
 
 ./fit_hvscan_spe -r ${run} -c "${CONNECTION_SET[$irun]}" -h ${HV_SET[$irun]}
 EOF
